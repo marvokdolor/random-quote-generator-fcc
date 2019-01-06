a67d8e169690dbd445b7a916ab9b3a04a6d7efd1
marvokdolor <marvourneen@mkdolor.com>
2019-01-06 18:07:47 -0500

    Updates

---
+++ precache-manifest.6e352ac2307fb3015f90a799ece65d88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee0581afc3ebf52a003b",
+    "url": "/random-quote-generator-fcc/static/js/runtime~main.ee0581af.js"
+  },
+  {
+    "revision": "24a99633193ce6309e61",
+    "url": "/random-quote-generator-fcc/static/js/main.24a99633.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/random-quote-generator-fcc/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "24a99633193ce6309e61",
+    "url": "/random-quote-generator-fcc/static/css/main.c1d611e4.chunk.css"
+  },
+  {
+    "revision": "d5e56708a9f7f3c6de20fedc7656cb07",
+    "url": "/random-quote-generator-fcc/index.html"
+  }
+];
